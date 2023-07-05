0a679be91fe94f0e6749040295501819064f9d9b
Ernesto Tejeda <ernesto.tejeda87@gmail.com>
2023-07-05 16:58:08 +0200

    [ADD] medical_center: new module.

---
+++ medical_center/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 MiEmpresa - Ernesto Tejeda
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "$ModuleName",
+    "version": "16.0.1.0.0",
+    "summary": "Medical center managements",
+    "category": "health",
+    "author": "Ernesto Tejeda",
+    "website": "https://github.com/ernestotejeda/odoo-examples",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/menus.xml",
+        "views/res_partner.xml",
+    ],
+}
